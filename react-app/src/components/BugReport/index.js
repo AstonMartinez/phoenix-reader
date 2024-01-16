ad626ea54460af5b1235c2dd70979ae7a745acfe
Aston Martinez <aston.martinez1614@gmail.com>
2024-01-16 16:12:34 -0600

    added some components to fill links in footer

---
+++ react-app/src/components/BugReport/index.js
@@ -0,0 +1,11 @@
+import "./BugReport.css";
+
+const BugReport = () => {
+  return (
+    <div>
+      <h2>Bug Report</h2>
+    </div>
+  );
+};
+
+export default BugReport;
---
+++ react-app/src/components/FeatureRequest/index.js
@@ -0,0 +1,11 @@
+import "./FeatureRequest.css";
+
+const FeatureRequest = () => {
+  return (
+    <div>
+      <h2>Feature Request</h2>
+    </div>
+  );
+};
+
+export default FeatureRequest;
